7db2994460e122d06780ea0758cb82e1fdd316c3
Kamil Raczycki <raczyckikamil@gmail.com>
2021-12-04 23:10:49 +0100

    Added Day 2 solutions

---
+++ src/app/solutions2021/day2.service.ts
@@ -0,0 +1,62 @@
+import { Injectable } from '@angular/core';
+import { BaseSolutionService } from '../helper/services/base-solution.service';
+import { ISolutionService } from '../helper/services/isolution.service';
+import { SolutionsCollectorService } from '../helper/services/solutions-collector.service';
+import { parseIntoNumbers } from '../helper/util-functions/parse-into-numbers';
+import { splitIntoLines } from '../helper/util-functions/split-into-lines';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class Day2Service extends BaseSolutionService implements ISolutionService {
+  constructor(solutionsCollectorService: SolutionsCollectorService) {
+    super(solutionsCollectorService, 2021, 2, 'Dive!');
+  }
+  private parseCommands(input: string): [string, number][] {
+    var commands = splitIntoLines(input);
+    return commands.map(command => {
+      var parts = command.split(' ');
+      return [parts[0], Number(parts[1])];
+    })
+  }
+  override solvePart1(input: string): string | number {
+    var commands = this.parseCommands(input);
+    var depth = 0;
+    var position = 0;
+    commands.forEach(command => {
+      switch(command[0]) {
+        case 'forward':
+          position += command[1];
+          break;
+        case 'up':
+          depth -= command[1];
+          break;
+        case 'down':
+          depth += command[1];
+          break;
+      }
+    });
+    return depth * position;
+  }
+  override solvePart2(input: string): string | number{
+    var commands = this.parseCommands(input);
+    var depth = 0;
+    var position = 0;
+    var aim = 0;
+    commands.forEach(command => {
+      switch(command[0]) {
+        case 'forward':
+          position += command[1];
+          depth += command[1] * aim;
+          break;
+        case 'up':
+          aim -= command[1];
+          break;
+        case 'down':
+          aim += command[1];
+          break;
+      }
+    });
+    return depth * position;
+  }
+}
--- src/app/solutions2021/solutions2021.module.ts
+++ src/app/solutions2021/solutions2021.module.ts
@@ -1,8 +1,9 @@
 import { APP_INITIALIZER, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HelperModule } from '../helper/helper.module';
-import { Day1Service } from './day1.service';
 import { ISolutionService } from '../helper/services/isolution.service';
+import { Day1Service } from './day1.service';
+import { Day2Service } from './day2.service';
 
 @NgModule({
   id: '2021',
@@ -13,6 +14,12 @@ import { ISolutionService } from '../helper/services/isolution.service';
       provide: APP_INITIALIZER,
       multi: true,
       deps: [Day1Service],
+      useFactory: (day1Service: ISolutionService) => () => null,
+    },
+    {
+      provide: APP_INITIALIZER,
+      multi: true,
+      deps: [Day2Service],
       useFactory: (day1Service: ISolutionService) => () => null,
     },
   ],
