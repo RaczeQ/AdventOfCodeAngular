e614758fc3da48f1aabdebbc0187a257a12b5915
Kamil Raczycki <raczyckikamil@gmail.com>
2021-12-05 00:37:20 +0100

    Added Day 3 solutions

--- src/app/solutions2021/solutions2021.module.ts
+++ src/app/solutions2021/solutions2021.module.ts
@@ -4,6 +4,7 @@ import { HelperModule } from '../helper/helper.module';
 import { ISolutionService } from '../helper/services/isolution.service';
 import { Day1Service } from './day1.service';
 import { Day2Service } from './day2.service';
+import { Day3Service } from './day3.service';
 
 @NgModule({
   id: '2021',
@@ -20,6 +21,12 @@ import { Day2Service } from './day2.service';
       provide: APP_INITIALIZER,
       multi: true,
       deps: [Day2Service],
+      useFactory: (day1Service: ISolutionService) => () => null,
+    },
+    {
+      provide: APP_INITIALIZER,
+      multi: true,
+      deps: [Day3Service],
       useFactory: (day1Service: ISolutionService) => () => null,
     },
   ],
