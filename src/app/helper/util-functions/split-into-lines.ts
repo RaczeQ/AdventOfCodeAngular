416b55a1f19673bd9bec55046f2217724c6d0ee0
Kamil Raczycki <raczyckikamil@gmail.com>
2021-12-12 18:56:04 +0100

    Fixed lines parsing if last one is empty

--- src/app/helper/util-functions/split-into-lines.ts
+++ src/app/helper/util-functions/split-into-lines.ts
@@ -1,3 +1,7 @@
 export function splitIntoLines(input: string): string[] {
-  return input.split('\n');
+  var lines = input.split('\n');
+  if (lines[lines.length - 1].trim().length == 0) {
+    lines = lines.slice(0, lines.length - 1);
+  }
+  return lines;
 }
