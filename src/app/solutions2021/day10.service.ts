3c8bb5912aebb32144b07c68cc56dd64932ca7b6
Kamil Raczycki <raczyckikamil@gmail.com>
2021-12-14 21:38:16 +0100

    Removed console.log from Day 10 solution

--- src/app/solutions2021/day10.service.ts
+++ src/app/solutions2021/day10.service.ts
@@ -116,7 +116,6 @@ export class Day10Service
         });
       }
       if (!isCorrupted) {
-        console.log(lineCopy);
         calculatedScores.push(
           lineCopy
             .split('')
