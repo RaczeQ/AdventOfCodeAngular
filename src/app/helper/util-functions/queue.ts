6276864e928a6536f05873a1a1994a9dda2cdd89
Kamil Raczycki <raczyckikamil@gmail.com>
2021-12-16 12:29:08 +0100

    Added queue data class to utility functions

---
+++ src/app/helper/util-functions/queue.ts
@@ -0,0 +1,12 @@
+export class Queue<T> {
+  _store: T[] = [];
+  isEmpty(): boolean {
+    return this._store.length == 0;
+  }
+  enqueue(val: T) {
+    this._store.push(val);
+  }
+  dequeue(): T | undefined {
+    return this._store.shift();
+  }
+}
