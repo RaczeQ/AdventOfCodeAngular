c511f168acb5249c8141c0859ab6721325eeb2f4
Kamil Raczycki <kar@dsr.com.pl>
2022-01-02 13:34:06 +0100

    Day 23 Part 2 progress

--- src/app/solutions2021/day23.service.ts
+++ src/app/solutions2021/day23.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { map } from 'rxjs';
 import { PlotlyGraphComponent } from '../helper/components/plotly-graph/plotly-graph.component';
 import { BaseSolutionService } from '../helper/services/base-solution.service';
 import {
@@ -40,18 +41,18 @@ export class Day23Service
     ['D', 9],
   ];
 
-  private calculatedCosts: { [idx: number]: { [char: string]: number } } =
-    {};
+  private calculatedCosts: { [idx: number]: { [char: string]: number } } = {};
 
-  private calculatedDistances: { [startIdx: number]: { [destIdx: number]: number } } =
-    {};
+  private calculatedDistances: {
+    [startIdx: number]: { [destIdx: number]: number[] };
+  } = {};
 
   private parseInput(lines: string[]): [Field[], Point2D[]] {
     var fields: Field[] = [];
     var positions: Point2D[] = [];
     lines.forEach((line, y) => {
       line.split('').forEach((char, x) => {
-        if (char != ' ' && char != '#') {
+        if (char.trim() != '' && char != '#') {
           positions.push({ x, y });
           var field: Field = {
             allowedStop: !(y > 1 || [3, 5, 7, 9].includes(x)),
@@ -112,40 +113,7 @@ export class Day23Service
     );
   }
 
-  private getValidNeighbours(
-    fieldIdx: number,
-    fields: Field[],
-    neighbours: { [id: number]: number[] }
-  ): number[] {
-    var result: number[] = [];
-    var field = fields[fieldIdx];
-    var priorityFinish = false;
-    neighbours[fieldIdx].forEach((nIdx) => {
-      if (!priorityFinish) {
-        var n = fields[nIdx];
-        if (!n.occupant) {
-          if (n.allowedFinish == field.occupant && nIdx > fieldIdx) {
-            if (this.checkIfLaneIsValid(fieldIdx, fields, neighbours)) {
-              priorityFinish = true;
-              result = [nIdx];
-            }
-            // console.log('Can finish');
-            result.push(nIdx);
-          } else if (n.allowedStop) {
-            // console.log('Allowed stop');
-            result.push(nIdx);
-          } else if (
-            !n.allowedStop &&
-            neighbours[nIdx].some((nPrimIdx) => !fields[nPrimIdx].occupant)
-          ) {
-            // console.log("Can't stop but sides free");
-            result.push(nIdx);
-          }
-        }
-      }
-    });
-    return result;
-  }
+  // private pathsCache: { [key: string]: [number, number][] } = {};
 
   private generateAllPaths(
     fieldIdx: number,
@@ -178,7 +146,6 @@ export class Day23Service
           );
         });
       if (!previousFieldIdx && result.length > 0) {
-        // console.log(fieldIdx, currentField.occupant, result);
         result = result.filter((r) => {
           var canMoveToTheEnd =
             Math.max(...neighbours[r[0]]) < r[0] &&
@@ -186,42 +153,18 @@ export class Day23Service
           var canFillRoom =
             fields[r[0]].allowedFinish == currentField.occupant &&
             this.checkIfLaneIsValid(r[0], fields, neighbours);
-          // Math.max(...neighbours[r[0]]) > r[0] &&
-          // fields[Math.max(...neighbours[r[0]])].allowedFinish ==
-          //   currentField.occupant &&
-          // fields[Math.max(...neighbours[r[0]])].occupant ==
-          //   currentField.occupant &&
-          // currentField.allowedFinish != currentField.occupant;
-          // console.log(
-          //   fieldIdx,
-          //   r[0],
-          //   canMoveToTheEnd,
-          //   canFillRoom,
-          //   currentField.allowedFinish && fields[r[0]].allowedStop
-          // );
-          // if (canFillRoom) {
-          //   console.log(
-          //     neighbours[r[0]],
-          //     fields[Math.max(...neighbours[r[0]])],
-          //     Math.max(...neighbours[r[0]]),
-          //     fields[r[0]].allowedFinish == currentField.occupant,
-          //     Math.max(...neighbours[r[0]]) > r[0],
-          //     fields[Math.max(...neighbours[r[0]])].allowedFinish ==
-          //       currentField.occupant,
-          //     currentField.allowedFinish != currentField.occupant
-          //   );
-          // }
           return (
             canMoveToTheEnd ||
             canFillRoom ||
             (currentField.allowedFinish && fields[r[0]].allowedStop)
           );
         });
-        // console.log(fieldIdx, currentField.occupant, result);
       }
     }
     return result;
   }
+
+  private movesCache: { [key: string]: [string, number][] } = {};
 
   private validMoves(
     fields: Field[],
@@ -230,13 +173,26 @@ export class Day23Service
     var result: [Field[], number][] = [];
     fields.forEach((field, fieldIdx) => {
       if (field.occupant) {
+        var subResult: [Field[], number][] = [];
+        // var key = this.generateHash(fieldIdx, fields, neighbours);
+        // if (key in this.movesCache) {
+        //   this.movesCache[key].forEach(([state, cost]) =>
+        //     result.push([JSON.parse(state) as Field[], cost])
+        //   );
+        // } else {
         var paths = this.generateAllPaths(fieldIdx, fields, neighbours);
         paths.forEach(([nIdx, cost]) => {
-          result.push([
+          subResult.push([
             this.swapPositions(fields, fieldIdx, nIdx),
             cost * this.costs[field.occupant!],
           ]);
         });
+        // this.movesCache[key] = subResult.map(([state, cost]) => [
+        //   JSON.stringify(state),
+        //   cost,
+        // ]);
+        result.push(...subResult);
+        // }
       }
     });
 
@@ -259,12 +215,12 @@ export class Day23Service
     if (key in this.heuristicCache) {
       return this.heuristicCache[key];
     }
-    var costs = fields
-      .map((f, idx) => {
-        return { f, idx };
-      })
-      .filter((t) => t.f.occupant && t.f.occupant != t.f.allowedFinish)
-      .map((t) => this.calculatedDistances[t.idx][t.f.occupant!]);
+    // var costs = fields
+    //   .map((f, idx) => {
+    //     return { f, idx };
+    //   })
+    //   .filter((t) => t.f.occupant && t.f.occupant != t.f.allowedFinish)
+    //   .map((t) => this.calculatedCosts[t.idx][t.f.occupant!]);
 
     var gapsCost = fields
       .map((f, idx) => {
@@ -280,7 +236,9 @@ export class Day23Service
       )
       .map((t) => this.costs[t.f.allowedFinish!]);
 
-    var corridors = fields.filter((f) => !f.allowedFinish).length;
+    // var corridors = fields.filter((f) => !f.allowedFinish).length;
+    var corridorsCost =
+      fields.filter((f) => !f.allowedFinish && f.occupant).length * 10000;
     var wrongFinishesCost = fields
       .map((f, idx) => {
         return { f, idx };
@@ -289,44 +247,91 @@ export class Day23Service
         (t) =>
           t.f.allowedFinish && t.f.occupant && t.f.occupant != t.f.allowedFinish
       )
-      .map((t) => {
-        return { f: t.f, d: Math.floor((t.idx - corridors) / 4) + 2 };
-      })
-      .map((t) => this.costs[t.f.occupant!] * t.d);
+      .map((t) => this.costs[t.f.allowedFinish!]);
 
-    var blockedFinishesCost = fields
+    // var blockedFinishesCost = fields
+    //   .map((f, idx) => {
+    //     return { f, idx };
+    //   })
+    //   .filter(
+    //     (t) =>
+    //       t.f.allowedFinish &&
+    //       t.f.occupant &&
+    //       // t.f.occupant == t.f.allowedFinish &&
+    //       !this.checkIfLaneIsValid(t.idx, fields, neighbours)
+    //   )
+    //   .map((t) => {
+    //     return { f: t.f, d: Math.floor((t.idx - corridors) / 4) + 2 };
+    //   })
+    //   .map((t) => this.costs[t.f.allowedFinish!] * t.d);
+
+    var paths = fields
       .map((f, idx) => {
         return { f, idx };
       })
       .filter(
         (t) =>
-          t.f.allowedFinish &&
           t.f.occupant &&
-          // t.f.occupant == t.f.allowedFinish &&
-          !this.checkIfLaneIsValid(t.idx, fields, neighbours)
+          fields.some(
+            (f) =>
+              f.allowedFinish == t.f.occupant && f.allowedFinish != f.occupant
+          )
       )
       .map((t) => {
-        return { f: t.f, d: Math.floor((t.idx - corridors) / 4) + 2 };
-      })
-      .map((t) => this.costs[t.f.allowedFinish!] * t.d);
+        return {
+          f: t.f,
+          path: this.calculatedDistances[t.idx][
+            Math.max(
+              ...fields
+                .map((f, idx) => {
+                  return { f, idx };
+                })
+                .filter(
+                  (tprim) =>
+                    tprim.f.allowedFinish == t.f.occupant &&
+                    tprim.f.allowedFinish != tprim.f.occupant
+                )
+                .map((t) => t.idx)
+            )
+          ],
+        };
+      });
 
-    // console.log(
-    //   this.printState(fields),
-    //   costs,
-    //   gapsCost,
-    //   wrongFinishesCost,
-    //   blockedFinishesCost
-    // );
+    var baseCosts = paths.map(({ f, path }) => {
+      return this.costs[f.occupant!] * path.length;
+    });
+
+    var occupiedCosts = paths
+      .map(({ f, path }) => {
+        return path
+          .filter(
+            (idx) => fields[idx].occupant && fields[idx].occupant != f.occupant
+          )
+          .map((idx) => this.costs[fields[idx].allowedFinish ?? f.occupant!]);
+      })
+      .flat();
+
+    // var blockedPathsCosts =
+
+    // console.log(this.printState(fields), paths, baseCosts, occupiedCosts);
+    // var result = costs.length > 0 ? costs.reduce((a, b) => a + b) : 0;
+    // var result = baseCosts.length > 0 ? baseCosts.reduce((a, b) => a + b) : 0;
+    // + (occupiedCosts.length > 0 ? occupiedCosts.reduce((a, b) => a + b) : 0);
 
     var result =
-      (costs.length > 0 ? costs.reduce((a, b) => a + b) : 0) +
+      (baseCosts.length > 0 ? baseCosts.reduce((a, b) => a + b) : 0) +
       (gapsCost.length > 0 ? gapsCost.reduce((a, b) => a + b) : 0) +
+      (occupiedCosts.length > 0 ? occupiedCosts.reduce((a, b) => a + b) : 0) +
       (wrongFinishesCost.length > 0
         ? wrongFinishesCost.reduce((a, b) => a + b)
         : 0) +
-      (blockedFinishesCost.length > 0
-        ? blockedFinishesCost.reduce((a, b) => a + b)
-        : 0);
+      corridorsCost;
+    //   (wrongFinishesCost.length > 0
+    //     ? wrongFinishesCost.reduce((a, b) => a + b)
+    //     : 0) +
+    //   (blockedFinishesCost.length > 0
+    //     ? blockedFinishesCost.reduce((a, b) => a + b)
+    //     : 0);
     this.heuristicCache[key] = result;
     return result;
   }
@@ -336,7 +341,7 @@ export class Day23Service
   aStar(
     fields: Field[],
     neighbours: { [id: number]: number[] },
-    printProgress: boolean = false
+    printProgress: number = -1
   ): {
     dist: number[];
     parents: (number | undefined)[];
@@ -360,6 +365,7 @@ export class Day23Service
     //start node has a priority equal to straight line distance to goal. It will be the first to be expanded.
     priorities[0] = this.heuristic(states[0], neighbours);
 
+    var counter = 0;
     //While there are nodes left to visit...
     while (true) {
       // ... find the node with the currently lowest priority...
@@ -421,8 +427,9 @@ export class Day23Service
       visited[lowestPriorityIndex] = true;
       totalVisited++;
 
-      if (printProgress) {
+      if (counter % printProgress == 0) {
         console.log(
+          counter,
           'states:',
           states.length,
           'idx:',
@@ -431,26 +438,134 @@ export class Day23Service
           priorities[lowestPriorityIndex],
           'dist:',
           distances[lowestPriorityIndex],
-          'vis:',
-          totalVisited,
-          totalVisited / states.length,
+          'vis%:',
+          Math.round((totalVisited * 100) / states.length),
           this.printState(states[lowestPriorityIndex])
         );
       }
+      counter++;
     }
   }
 
-  private calculateDistances(positions: Point2D[]) {
+  private calculateCosts(positions: Point2D[]) {
     positions.forEach((pos, posIdx) => {
-      this.calculatedDistances[posIdx] = {};
+      this.calculatedCosts[posIdx] = {};
       this.positions.forEach(([char, finalX]) => {
         var xDiff = Math.abs(pos.x - finalX);
-        this.calculatedDistances[posIdx][char] =
+        this.calculatedCosts[posIdx][char] =
           xDiff > 0 || pos.y == 1
             ? (xDiff + (pos.y - 1) + 2) * this.costs[char]
             : 0;
       });
     });
+  }
+
+  private calculateDistances(
+    fields: Field[],
+    neighbours: { [id: number]: number[] }
+  ) {
+    function explorePaths(
+      fieldIdx: number,
+      previousFieldIdx: number
+    ): number[][] {
+      var result: number[][] = [];
+      neighbours[fieldIdx]
+        .filter((nIdx) => nIdx != previousFieldIdx)
+        .forEach((nIdx) => {
+          var path = [nIdx];
+          result.push(path);
+          explorePaths(nIdx, fieldIdx).forEach((deepPath) => {
+            result.push(path.concat(deepPath));
+          });
+        });
+      // console.log(result);
+      return result;
+    }
+    fields.forEach((field, fieldIdx) => {
+      this.calculatedDistances[fieldIdx] = {};
+      explorePaths(fieldIdx, -1).forEach((path) => {
+        // console.log(fieldIdx, path);
+        var endIdx = path.slice(-1)[0];
+        var cutPath = path.slice(0, path.length - 1);
+        // console.log(fieldIdx, cutPath, endIdx);
+        if (!(fieldIdx in this.calculatedDistances)) {
+          this.calculatedDistances[fieldIdx] = {};
+        }
+        if (!(endIdx in this.calculatedDistances)) {
+          this.calculatedDistances[endIdx] = {};
+        }
+        this.calculatedDistances[fieldIdx][endIdx] = [...cutPath].concat([
+          endIdx,
+        ]);
+        this.calculatedDistances[endIdx][fieldIdx] = cutPath
+          .reverse()
+          .concat([fieldIdx]);
+      });
+    });
+  }
+
+  private generateHash(
+    fieldIdx: number,
+    fields: Field[],
+    neighbours: { [id: number]: number[] }
+  ): string {
+    var field = fields[fieldIdx];
+    var freeIdxs: number[] = [];
+    var nextNeighbours = [...neighbours[fieldIdx]];
+    while (nextNeighbours.length > 0) {
+      var tempNextNeighbours: number[] = [];
+      nextNeighbours.forEach((nIdx) => {
+        if (
+          !fields[nIdx].occupant
+          // &&
+          // (!fields[nIdx].allowedFinish ||
+          //   fields[nIdx].allowedFinish == field.occupant)
+        ) {
+          freeIdxs.push(nIdx);
+          neighbours[nIdx]
+            .filter((nPrim) => !freeIdxs.includes(nPrim))
+            .forEach((nPrim) => {
+              tempNextNeighbours.push(nPrim);
+            });
+        }
+      });
+      nextNeighbours = tempNextNeighbours;
+    }
+    var alteredFields = fields.map((f, idx) => {
+      return {
+        f,
+        letter:
+          idx == fieldIdx
+            ? f.occupant!
+            : freeIdxs.includes(idx)
+            ? ' '
+            : fields[idx].occupant == field.occupant
+            ? f.occupant!.toLowerCase()
+            : 'X',
+      };
+    });
+    // return alteredFields.map(({ f, letter }) => letter).join('');
+    var lane = alteredFields
+      .filter(({ f, letter }) => !f.allowedFinish)
+      .map(({ f, letter }) => letter)
+      .join('');
+    var roomA = alteredFields
+      .filter(({ f, letter }) => f.allowedFinish == 'A')
+      .map(({ f, letter }) => letter)
+      .join('');
+    var roomB = alteredFields
+      .filter(({ f, letter }) => f.allowedFinish == 'B')
+      .map(({ f, letter }) => letter)
+      .join('');
+    var roomC = alteredFields
+      .filter(({ f, letter }) => f.allowedFinish == 'C')
+      .map(({ f, letter }) => letter)
+      .join('');
+    var roomD = alteredFields
+      .filter(({ f, letter }) => f.allowedFinish == 'D')
+      .map(({ f, letter }) => letter)
+      .join('');
+    return `(${lane})[${roomA}][${roomB}][${roomC}][${roomD}]`;
   }
 
   private printState(fields: Field[]): string {
@@ -461,19 +576,19 @@ export class Day23Service
     var roomA = fields
       .filter((f) => f.allowedFinish == 'A')
       .map((f) => f.occupant ?? ' ')
-      .join();
+      .join('');
     var roomB = fields
       .filter((f) => f.allowedFinish == 'B')
       .map((f) => f.occupant ?? ' ')
-      .join();
+      .join('');
     var roomC = fields
       .filter((f) => f.allowedFinish == 'C')
       .map((f) => f.occupant ?? ' ')
-      .join();
+      .join('');
     var roomD = fields
       .filter((f) => f.allowedFinish == 'D')
       .map((f) => f.occupant ?? ' ')
-      .join();
+      .join('');
     return `(${lane})[${roomA}][${roomB}][${roomC}][${roomD}]`;
   }
 
@@ -481,17 +596,22 @@ export class Day23Service
     var lines = splitIntoLines(input);
     var [fields, positions] = this.parseInput(lines);
     var neighbours = this.getNeighbourhoods(positions);
-    this.calculateDistances(positions);
+    this.calculateCosts(positions);
+    this.calculateDistances(fields, neighbours);
+    // console.log(this.calculatedDistances);
+    // console.log(this.calculatedCosts);
+    // console.log(this.printState(fields), this.heuristic(fields, neighbours));
     // console.log(fields, neighbours);
     // console.log(this.printState(fields));
     // console.log(this.calculatedDistances);
     // console.log(this.heuristic(fields));
     // console.log(this.validMoves(fields, neighbours));
+    // console.log(this.movesCache);
 
     var { dist, parents, states, finalStateIdx } = this.aStar(
       fields,
       neighbours,
-      true
+      1
     );
     var path = [finalStateIdx];
     while (path[path.length - 1] !== undefined && path[path.length - 1] != 0) {
@@ -499,7 +619,9 @@ export class Day23Service
     }
     path.reverse();
     console.log(finalStateIdx, path);
-    path.forEach((p) => console.log(this.printState(states[p])));
+    path.forEach((p) => {
+      console.log(this.printState(states[p]), dist[p]);
+    });
     return {
       result: dist[finalStateIdx],
       // result: 0,
@@ -513,20 +635,24 @@ export class Day23Service
     lines.splice(4, 0, '  #D#B#A#C#');
     var [fields, positions] = this.parseInput(lines);
     var neighbours = this.getNeighbourhoods(positions);
-    console.log(this.printState(fields));
-    this.calculateDistances(positions);
+    // console.log(this.printState(fields));
+    this.calculateCosts(positions);
+    this.calculateDistances(fields, neighbours);
+    // console.log(this.calculatedDistances);
+    // console.log(this.calculatedCosts);
     // console.log(fields, neighbours);
     // console.log(this.printState(fields));
-    console.log(this.calculatedDistances);
-    console.log(this.printState(fields), this.heuristic(fields, neighbours));
-    console.log(this.validMoves(fields, neighbours));
-    this.validMoves(fields, neighbours).forEach((t) => {
-      console.log(this.printState(t[0]), this.heuristic(t[0], neighbours));
-    });
+    // console.log(this.printState(fields), this.heuristic(fields, neighbours));
+    // console.log(this.validMoves(fields, neighbours));
+    // console.log(this.movesCache);
+    // this.validMoves(fields, neighbours).forEach((t) => {
+    //   console.log(this.printState(t[0]), this.heuristic(t[0], neighbours));
+    // });
+
     var { dist, parents, states, finalStateIdx } = this.aStar(
       fields,
       neighbours,
-      true
+      100
     );
     console.log(dist, parents, states, finalStateIdx);
     var path = [finalStateIdx];
