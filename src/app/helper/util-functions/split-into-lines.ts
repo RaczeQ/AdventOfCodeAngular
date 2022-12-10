bfc653cdd161cea5fb957cd4064fc550e1f42a0d
Kamil Raczycki <raczyckikamil@gmail.com>
2022-12-10 17:10:08 +0100

    fix: added trimming to all lines of parsed input

--- src/app/helper/util-functions/split-into-lines.ts
+++ src/app/helper/util-functions/split-into-lines.ts
@@ -1,6 +1,6 @@
 export function splitIntoLines(input: string): string[] {
-  var lines = input.split('\n');
-  if (lines[lines.length - 1].trim().length == 0) {
+  var lines = input.split('\n').map((l) => l.trim());
+  if (lines[lines.length - 1].length == 0) {
     lines = lines.slice(0, lines.length - 1);
   }
   return lines;
